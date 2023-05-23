cf3ffb40516b0a0a372396fe49a9a8fb3dcbc1ae
roscibely <rosanacibely@gmail.com>
2023-05-23 15:55:24 -0300

    feat: hellodjango project

--- hellodjango/hellodjango/urls.py
+++ hellodjango/hellodjango/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for hellodjango project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
